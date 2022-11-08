e0fe4962df90e6524bf18b531cae758d8801a102
masa <m.moriai.sys22@morijyobi.ac.jp>
2022-11-08 10:17:33 +0900

    修正

--- Main.java
+++ Main.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
 		double weight = 0;
+		double weight2 = 0;
 		double height = 0;
+		double height2 = 0;
 		double answer = 0;
+		double answer2 = 0;
 		Scanner sc = new Scanner(System.in);
 		answer = weight/(height*height);
+		answer2 = weight2/(height2*height2);
 		System.out.println(String.format("%.2f",answer));
+		System.out.println(String.format("%.2f",answer2));
 		}
 }
 				
